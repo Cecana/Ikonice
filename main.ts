d81f661859d8ba0378791d629fc0b6c7e5a8d90a
Svetlana Milutinović <cecanam@gmail.com>
2020-12-13 16:16:09 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Giraffe)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Sword)
+})
